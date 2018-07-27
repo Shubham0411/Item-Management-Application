294ac1619299e9799781c17891917b76c891ba78
Shubham Agrawal <shubhu8800@gmail.com>
2018-07-27 18:27:47 +0530

    Update Program.cs

--- Inventory/Inventory/Program.cs
+++ Inventory/Inventory/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,29 +6,34 @@ using System.Threading.Tasks;
 
 namespace Inventory
 {
-     class ComputerProgram
+    //4 basic variables, they form the skeleton of code
+    abstract class MyAbstractDetails
     {
-        public string name,category;
+        public string name, category;
         public int code;
         public float size;
-        
 
-        //Method takes input of Computer Program Details from user
-        public void InputDetails() 
+        public void InputDetails()
         {
-            Console.WriteLine("Enter name");
+            Console.Write("Enter name :-");
             name = Console.ReadLine();
-            Console.WriteLine("Enter Code");
+            Console.Write("Enter Code:-");
             code = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter size");
+            Console.Write("Enter size:-");
             size = float.Parse(Console.ReadLine());
-            Console.WriteLine("Enter category");
+            Console.Write("Enter category:-");
             category = Console.ReadLine();
         }
+
     }
 
-    //Inherits the variables of Computer Program for it's use
-   class MusicFiles : ComputerProgram
+    class ComputerProgram:MyAbstractDetails
+    {
+        //First Class, extends MyAbstarctDetails to use the variables
+    }
+
+    //Inherits the variables of MyAbstractDetails for it's use
+   class MusicFiles : MyAbstractDetails
     {
         public string singer_name;
         public int length;
@@ -36,9 +41,9 @@ namespace Inventory
         // This method takes input from user regarding the details of the song
             public void MusicInputDetails()
         {
-            Console.WriteLine("Enter Singer name");
+            Console.Write("Enter Singer name:-");
             singer_name = Console.ReadLine();
-            Console.WriteLine("Enter Length of music");
+            Console.Write("Enter Length of music:-");
             length = Convert.ToInt32(Console.ReadLine());
         }
 
@@ -52,19 +57,19 @@ namespace Inventory
         }
     }
 
-    //Inherits Computer Program to use it's variables
-    class Films:ComputerProgram
+    //Inherits MyAbstractDetails to use it's variables
+    class Films:MyAbstractDetails
     {
         public string actor_name,director_name,actress_name;
         
         //This method asks the user for input
             public void FilmInputDetails()
         {
-            Console.WriteLine("Enter Actor name");
+            Console.Write("Enter Actor name:-");
             actor_name = Console.ReadLine();
-            Console.WriteLine("Actress name");
+            Console.Write("Actress name:-");
             actress_name = Console.ReadLine();
-            Console.WriteLine("Director name");
+            Console.Write("Director name:-");
             director_name = Console.ReadLine();
         }
 
@@ -90,35 +95,38 @@ namespace Inventory
             {
                 ComputerProgram computerProgram = new ComputerProgram();
                 Console.WriteLine(computerProgram.ToString());
-                Console.WriteLine("Computer Program" + i);
+                Console.WriteLine("Computer Program " + (1+i) +" ");
                 computerProgram.InputDetails();
                 Console.WriteLine("Name is " + computerProgram.name + " code is " + computerProgram.code +" size "+ computerProgram.size+" category "+ computerProgram.category);
                 Console.WriteLine("Press to continue");
                 Console.ReadKey();
+                Console.WriteLine("");
             }
             
             for (int i=0;i<filmsObject.Length;i++)
             {
                 Films films = new Films();
                 Console.WriteLine(films.ToString());
-                Console.WriteLine(" Films " + i);
+                Console.WriteLine(" Films " + (1+i)+" ");
                 films.InputDetails();
                 films.FilmInputDetails();
                 Console.WriteLine("Name is " + films.name + " code is " + films.code + " size " + films.size + " category " + films.category + " Actor name " + films.actor_name + " Actress name " + films.actress_name + " Director name " + films.director_name);
                 Console.WriteLine("Press to continue");
                 Console.ReadKey();
+                Console.WriteLine("");
             }
             
             for (int i =0;i<musicFilesObject.Length;i++)
             {
                 MusicFiles musicFiles = new MusicFiles();
                 Console.WriteLine(musicFiles.ToString());
-                Console.WriteLine("Music Files" + i);
+                Console.WriteLine("Music Files" + (1+i) + " ");
                 musicFiles.InputDetails();
                 musicFiles.MusicInputDetails();
                 Console.WriteLine("Name is " + musicFiles.name + " code is " + musicFiles.code + " size " + musicFiles.size + " category " + musicFiles.category+"Singer name"+ musicFiles.singer_name+"length"+ musicFiles.length);
                 Console.WriteLine("Press to continue");
                 Console.ReadKey();
+                Console.WriteLine("");
             }
         }
     }
